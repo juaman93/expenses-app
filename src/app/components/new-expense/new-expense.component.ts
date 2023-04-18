c4bc4077288384a0335f23d6ced4be86723be6ae
Juan Montealegre <juanmm990@gmail.com>
2023-04-17 19:19:37 -0500

    Added columns for expense details

--- src/app/components/new-expense/new-expense.component.ts
+++ src/app/components/new-expense/new-expense.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { ExpensesService } from 'src/app/services/expenses.service'
 
 @Component({
