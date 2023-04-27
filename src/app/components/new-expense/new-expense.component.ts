ccfe95266125b7ad9b1019f3ed40ea3c950d615a
Juan Montealegre <juanmm990@gmail.com>
2023-04-27 17:13:12 -0500

    format the amount input field of the new expense

--- src/app/components/new-expense/new-expense.component.ts
+++ src/app/components/new-expense/new-expense.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
 import { ExpensesService } from 'src/app/services/expenses.service'
+import { CurrencyPipe } from '@angular/common'
 
 @Component({
   selector: 'app-new-expense',
@@ -10,13 +11,36 @@ import { ExpensesService } from 'src/app/services/expenses.service'
 export class NewExpenseComponent implements OnInit {
   form: FormGroup
 
-  constructor(private expensesService: ExpensesService) {
+  constructor(
+    private expensesService: ExpensesService,
+    private currencyPipe: CurrencyPipe
+  ) {
     this.form = new FormGroup({
       description: new FormControl(),
       amount: new FormControl(),
       category: new FormControl(),
       date: new FormControl()
     })
+  }
+
+  formatInput(event: any) {
+    let value = event.target.value
+    // Remove any non-digit characters
+    value = value.replace(/[^0-9\.]+/g, '')
+    // Parse the string value to a number
+    let numValue = parseFloat(value)
+    if (isNaN(numValue)) {
+      // If the parsed value is NaN, set it to 0
+      numValue = 0
+    }
+    // Format the number as currency
+    const formattedValue = numValue.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2
+    })
+    event.target.value = formattedValue
   }
 
   ngOnInit() {}
