7170593285dc507945fd105e502ced48eaff8a55
Juan Montealegre <juanmm990@gmail.com>
2023-05-11 16:30:42 -0500

    create expense-list table

--- src/app/components/expense-list/expense-list.component.ts
+++ src/app/components/expense-list/expense-list.component.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit } from '@angular/core'
-import Expense from 'src/app/interfaces/expense.interface'
+import { Component, NgModule } from '@angular/core'
 import { ExpensesService } from 'src/app/services/expenses.service'
+import { DataSource } from '@angular/cdk/collections'
 
 @Component({
   selector: 'app-expense-list',
   templateUrl: './expense-list.component.html',
   styleUrls: ['./expense-list.component.scss']
 })
-export class ExpenseListComponent implements OnInit {
-  expenses: Expense[]
+export class ExpenseListComponent {
   displayedColumns: string[] = ['description', 'amount', 'category', 'date']
+  dataSource = new ExpenseDataSource(this.expense)
 
-  constructor(private expensesService: ExpensesService) {
-    this.expenses = []
+  constructor(private expense: ExpensesService) {}
+}
+
+export class ExpenseDataSource extends DataSource<any> {
+  constructor(private expense: ExpensesService) {
+    super()
   }
 
-  ngOnInit(): void {
-    this.expensesService.getExpenses().subscribe((expenses) => {
-      this.expenses = expenses
-    })
+  connect() {
+    return this.expense.getExpenses()
   }
 
-  async onClickDelete(expense: Expense) {
-    const response = await this.expensesService.deleteExpense(expense)
-    console.log(response)
-  }
+  disconnect() {}
 }
